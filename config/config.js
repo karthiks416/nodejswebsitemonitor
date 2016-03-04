3e4b03caa6ca6b8186dedf1ceb09225f49ba231c
karthiks416 <karthiks416@gmail.com>
2016-03-04 11:45:26 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ userId:"youremailid",
 password:"yourpassword",
 timeout:300000,
 service:'Gmail',
-subject:'**CRITICAL: SNAPS PROD SERVER MONITORING',
+subject:'**Website Monitor Alert',
 receipients:["youremailids"],
 text:" is down. The error code is "
 };
